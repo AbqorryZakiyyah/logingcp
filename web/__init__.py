6d83661a4d5ced7d2f2555ba9551c2032758078e
Abqorry Zakiyyah <125792538+AbqorryZakiyyah@users.noreply.github.com>
2023-06-13 12:41:52 +0700

    Add files via upload

---
+++ web/__init__.py
@@ -0,0 +1,52 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from os import path
+from flask_login import LoginManager
+
+db = SQLAlchemy()
+DB_NAME = "database.db"
+
+
+
+def create_app():
+    app = Flask(__name__)
+    
+    # Google Cloud SQL (change this accordingly)
+    PASSWORD ="1234567"
+    PUBLIC_IP_ADDRESS ="34.101.187.228"
+    DBNAME ="diary"
+    PROJECT_ID ="login-389703"
+    INSTANCE_NAME ="healthdiary"
+
+    app.config["SECRET_KEY"] = "healthdiary secretkey"
+    app.config["SQLALCHEMY_DATABASE_URI"]= f"mysql + mysqldb://root:{1234567}@{34.101.187.228}/{diary}?unix_socket =/cloudsql/{login-389703}:{healthdiary}"
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"]= True
+    db.init_app(app)
+
+    from .views import views
+    from .auth import auth
+
+    app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/')
+
+    from .models import User, Diary
+    
+    with app.app_context():
+        db.create_all()
+
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
+
+    return app
+
+
+def create_database(app):
+    if not path.exists('web/' + DB_NAME):
+        db.create_all(app=app)
+        print('Created Database!')
+
